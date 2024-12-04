18a4bd15c9d633f825a44f640035a47b59a25cd9
Hector Camilo Perez <hectorcperez21@gmail.com>
2024-12-04 16:20:37 -0500

    Configuración para despliegue en Netlify

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import Response
 
 def handler(event, context):
-    return app(event, context)
+    """Función handler para Netlify Functions"""
+    return Response(
+        app(event, context),
+        mimetype='text/html'
+    )
 
 if __name__ == "__main__":
     app.run()
